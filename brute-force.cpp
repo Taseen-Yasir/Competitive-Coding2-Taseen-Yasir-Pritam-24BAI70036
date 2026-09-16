d7a772f1b9ab9629c724e1fc846310c550320d8d
Taseen Yasir Pritam <yasirpritam9@gmail.com>
2026-09-16 14:48:53 +0530

    Add files via upload

---
+++ brute-force.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 2)
+        return n;
+
+    return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of stairs: ";
+    cin >> n;
+
+    cout << "Number of ways: " << climbStairs(n) << endl;
+
+    return 0;
+}
